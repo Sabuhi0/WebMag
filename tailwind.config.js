aa64cb3dcfdb552664cc41a54bfaa474dbb2dcba
Sabuhi0 <sabuhiq0@gmail.com>
2022-02-01 17:04:47 +0400

    Change content config js file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,8 @@
-module.exports = {  content: ["./src/**/*.{html,js}"],  theme: {    extend: {},  },  plugins: [],}
+module.exports = {
+    content: [
+        './components/**/*.{html,js}',
+        './pages/**/*.{html,js}',
+        './index.html',
+    ],
+    // ...
+}
